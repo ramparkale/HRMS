ae3d2c06495aa0445997248788cce76ff839edcc
ramparkale <parkaleram@gmail.com>
2026-07-25 10:32:42 +0530

    department

---
+++ HRMS/DTO/DepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HRMS.DTO
+{
+    public class DepartmentDto
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
