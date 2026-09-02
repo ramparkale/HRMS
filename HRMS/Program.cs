ad0c5b61ac7a5e6d87de1b29a85f8c9954b361d2
ramparkale <parkaleram@gmail.com>
2026-09-02 19:27:44 +0530

    ok

--- HRMS/Program.cs
+++ HRMS/Program.cs
@@ -118,12 +118,12 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseCors("AllowAll");
-}
+//}
 
 app.UseHttpsRedirection();
 
